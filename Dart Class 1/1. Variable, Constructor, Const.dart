b015ad6eedb6b3182a54ddde99f58568449f07bd
Hassan Osama <30869965+sudoosama@users.noreply.github.com>
2020-09-19 15:06:02 +0500

    Class 1 Dart

---
+++ Dart Class 1/1. Variable, Constructor, Const.dart
@@ -0,0 +1,15 @@
+void main() {
+  //TODO: Const, Constructor
+
+  const simpleVar = 'hello world';
+  const myList = [1, 2, 3];
+  const myMap = {'one': 1};
+  const mySet = {1, 2, 3};
+  print(simpleVar);
+  print(myList);
+  print(myMap);
+  print(mySet);
+
+  final myListf = [1, 2, 3];
+  print(myListf);
+}
---
+++ Dart Class 1/2. Type of List.dart
@@ -0,0 +1,15 @@
+void main() {
+  //TODO: Different types of List in Dart!
+
+  var citiesChina = <String>['Beijing', 'Shanghai'];
+  List<String> citiesAustralia = ['Perth', 'Canberra'];
+  print(citiesChina);
+  print(citiesAustralia);
+
+  const listOfNumbers = <int>[1, 2, 3];
+  const List<int> numbersOflist = [1, 2, 3];
+  print(numbersOflist + listOfNumbers);
+
+  final studentNames = <String>['Hassan', 'Osama'];
+  print(studentNames);
+}
